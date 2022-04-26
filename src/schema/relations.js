5a14f49db52030f18795bc73e9e1f14a0473844f
steinskeeper <dpelpsykongroo@gmail.com>
2022-04-27 01:15:25 +0530

    findOne

--- src/schema/relations.js
+++ src/schema/relations.js
@@ -2,7 +2,7 @@ import { UserTC } from '../models/user';
 import { LeadTC } from '../models/lead';
 
 LeadTC.addRelation('createdByUser', {
-  resolver: () => UserTC.getResolver('findById'),
+  resolver: () => UserTC.getResolver('findOne'),
   prepareArgs: {
     _id: source => source.userID
   },
