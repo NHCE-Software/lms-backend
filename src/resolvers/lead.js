ab6a0ec3ed474535249f9065c3eb8713e729b656
steinskeeper <dpelpsykongroo@gmail.com>
2022-04-27 12:43:04 +0530

    never using maps again

--- src/resolvers/lead.js
+++ src/resolvers/lead.js
@@ -14,28 +14,70 @@ const addLeads = {
     type: 'JSON',
     args: { record: '[JSON]' },
 
-    resolve: async ({ args,context:{user} }) => {
+    resolve: async ({ args, context: { user } }) => {
         try {
             let leads = args.record;
-            leads.map(async(lead) => {
-                const leadData =  await Lead.findOne({$or:[{email:lead.email},{phonenumber:lead.phonenumber}]});
-                
-                if(leadData){
-                    console.log("I am here")
-                    console.log(leadData)
-                    leadData.loadedby.indexOf(user._id.toString())===-1? leadData.loadedby.push(user._id):console.log("f")
-                    leadData.source.indexOf(lead.source)===-1? leadData.source.push(lead.source):console.log("j")
-                    leadData.course.indexOf(lead.course)===-1? leadData.course.push(lead.course):console.log("k")
-                    console.log(leadData)
-                    leadData.save();
-                }
-                else{
-                    console.log("I am there")
-                    lead.loadedby = [user._id];
-                    Lead.create(lead);
+            // leads.map(async (lead) => {
+            //     const leadData = await Lead.findOne({
+            //         $or: [
+            //             { email: lead.email },
+            //             { phonenumber: lead.phonenumber },
+            //         ],
+            //     });
 
-                }
-            });
+            //     if (leadData) {
+            //         console.log('I am here');
+            //         console.log(leadData);
+            //         leadData.loadedby.indexOf(user._id.toString()) === -1
+            //             ? leadData.loadedby.push(user._id)
+            //             : console.log('f');
+            //         leadData.source.indexOf(lead.source) === -1
+            //             ? leadData.source.push(lead.source)
+            //             : console.log('j');
+            //         leadData.course.indexOf(lead.course) === -1
+            //             ? leadData.course.push(lead.course)
+            //             : console.log('k');
+            //         console.log(leadData);
+            //         leadData.save();
+            //     } else {
+            //         console.log('I am there');
+            //         lead.loadedby = [user._id];
+            //         let newLead = new Lead({...lead})
+            //         await newLead.save()
+
+            //     }
+            // });
+            for(let i = 0;i<leads.length;i++){
+                const leadData = await Lead.findOne({
+                            $or: [
+                                { email: leads[i].email },
+                                { phonenumber: leads[i].phonenumber },
+                            ],
+                        });
+        
+                        if (leadData) {
+                            console.log('I am here');
+                            console.log(leadData);
+                            leadData.loadedby.indexOf(user._id.toString()) === -1
+                                ? leadData.loadedby.push(user._id)
+                                : console.log('f');
+                            leadData.source.indexOf(leads[i].source) === -1
+                                ? leadData.source.push(leads[i].source)
+                                : console.log('j');
+                            leadData.course.indexOf(leads[i].course) === -1
+                                ? leadData.course.push(leads[i].course)
+                                : console.log('k');
+                            console.log(leadData);
+                            leadData.save();
+                        } else {
+                            console.log('I am there');
+                            leads[i].loadedby = [user._id];
+                            let newLead = new Lead({...leads[i]})
+                            await newLead.save()
+        
+                        }
+
+            }
         } catch (error) {
             throw new Error(error);
         }
@@ -64,36 +106,35 @@ const addCall = {
                     },
                 }
             );
-            return{
-                message:"success"
-            }
+            return {
+                message: 'success',
+            };
         } catch (error) {}
     },
 };
 
-const getLeads  = {
+const getLeads = {
     name: 'getLeads',
     type: '[Lead]',
     args: { record: 'JSON' },
-    resolve: async ({ context: { user } }) => {
+    resolve: async ({ args, context: { user } }) => {
         try {
-            
-
-            if (user.role==="admin") {
-                console.log("heww")
+            if (user.role === 'admin') {
+                console.log('heww');
                 const leads = await Lead.find();
                 return leads;
+            } else if (user.role === 'admin' && args.record.callerid) {
+                const leads = await Lead.find({ _id: args.record.callerid });
+                return leads;
             } else {
-                
-                const leads = await Lead.find({loadedby:{$elemMatch:{$eq:user._id}}});
+                const leads = await Lead.find({
+                    loadedby: { $elemMatch: { $eq: user._id } },
+                });
                 return leads;
             }
-            
         } catch (error) {}
     },
 };
-
-
 
 module.exports = {
     addLeads,
